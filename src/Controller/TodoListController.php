85ed62a31a331997cd32873a2f6b535d01019207
Guilherme Perim <gui_pab@hotmail.com>
2019-12-22 07:33:50 -0300

    Commit:  -> Implementação do método de update da tarefa  -> IMplementação da listagem de tarefas cadastradas

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Tasks;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoListController extends AbstractController
@@ -12,17 +14,62 @@ class TodoListController extends AbstractController
      */
     public function index()
     {
+        $tasks = $this->getDoctrine()->getRepository(Tasks::class)->findBy([], ['id' => 'DESC']);
+
         return $this->render('todo_list/index.html.twig', [
-            'controller_name' => 'TodoListController',
+            'tasks' => $tasks,
         ]);
     }
 
     /**
      * @Route("/create", name="create_task", methods={"post"})
      */
-    public function create()
+    public function create(Request $request)
     {
-        exit('to do: create a new task');
+        //Recuperamos o valor enviado através do método POST
+        $title = $request->request->get('title');
+
+        //Se o valor enviado for nulo, redirecionamos para á página inicial
+        if (empty($title)) {
+            $this->addFlash(
+                'warning',
+                'Informe um título válido para a tarefa'
+            );
+        }
+        else {
+            try {
+                //Criamos o gerenciador da entidade
+                $entityManager = $this->getDoctrine()->getManager();
+
+                //Criamos o objeto tasks e setamos seus valores
+                $tasks = new Tasks;
+                $tasks->setTitle($title);
+                $tasks->setStatus(0);
+
+                //Persistimos as informação na base de dados
+                $entityManager->persist($tasks);
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Tarefa cadastrada!'
+                );
+            }
+            catch(DBALException $e){
+                $this->addFlash(
+                    'error',
+                    $e->getMessage()
+                );
+            }
+            catch(\Exception $e){
+                $this->addFlash(
+                    'error',
+                    $e->getMessage()
+                );
+            }
+        }
+
+        return $this->redirectToRoute('todo_list');
     }
 
     /**
@@ -30,7 +77,31 @@ class TodoListController extends AbstractController
      */
     public function switchStatus($id)
     {
-        exit('to do: update the status of a task'.$id);
+        try {
+            //Criamos o gerenciador da entidade
+            $entityManager = $this->getDoctrine()->getManager();
+            $task = $entityManager->getRepository(Tasks::class)->find($id);
+            $task->setStatus(!$task->getStatus());
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Tarefa atualizada!'
+            );
+        }
+        catch(DBALException $e){
+            $this->addFlash(
+                'error',
+                $e->getMessage()
+            );
+        }
+        catch(\Exception $e){
+            $this->addFlash(
+                'error',
+                $e->getMessage()
+            );
+        }
+
+        return $this->redirectToRoute('todo_list');
     }
 
     /**
